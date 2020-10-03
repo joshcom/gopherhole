d70bbb00840cb7f993bc6db84d6a982c7e6ccd2b
Joshua Murray <joshua.murray@gmail.com>
2020-10-03 11:54:41 +0000

    Config checking

--- TODO.md
+++ TODO.md
@@ -1,12 +1,12 @@
 # TODO
 
 ## Additional TODOs
-1. What happens with various invalid config settings? (selector and root path, mime type)
-2. Review the period situation (https://tools.ietf.org/html/rfc1436)
-3. Make sure all file absolute paths are in root (and make an absolute path out of the provided root config).
-4. Coverage report and CI
+1. Update documentation
+2. Coverage report and CI
 
 ## Future projects
+* Additional resource checks: symlink detection and per-file path validation.
+* Validate configuration before server starts.
 * Streaming writes, instead of loading the file in memory.
 * Additional mime-types file. (/etc/mime.types)
 * Use channel, instead of sleep, when connections max out.
--- gopherhole/selector.go
+++ gopherhole/selector.go
@@ -19,21 +19,30 @@ func newSelector(rootPath string, query string) selector {
 }
 
 func (s *selector) path() (fullPath string, err error) {
+	// In the future, this check ought to be made during a config
+	// validation check.
 	if strings.TrimSpace(s.rootPath) == "" {
 		return fullPath, errors.New("Root path cannot be blank.")
 	}
 
+	// Make an absolute path out of the root path configured for
+	// this server.  This will change relative paths to absolute
+	// paths, for the check that comes up next.
 	absRoot, err := filepath.Abs(s.rootPath)
 	if err != nil {
 		return
 	}
 
+	// Make a full, and absolute, path out of the root path
+	// + the query ('/var/gopher' + '/phlog' = '/var/gopher/phlog').
 	fullPath = absRoot + "/" + s.query
 	fullPath, err = filepath.Abs(fullPath)
 	if err != nil {
 		return
 	}
 
+	// Finally, ensure the derived path to the file is within the
+	// root directory.
 	if strings.Index(fullPath, absRoot) != 0 {
 		err = fmt.Errorf("Evaluated path %s does not begin with specified root %s",
 			fullPath,
